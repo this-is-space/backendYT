9e4ab544fa8446a9948ea7645c545e5582c36ee7
Space <maperdj92@gmail.com>
2024-03-27 17:50:20 +0530

    Added data Models for User and Video

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 process.loadEnvFile(".env");
 
-import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import express from "express";
 
-connectDB();
+const app = express();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB Connection Failed ", err);
+  });
